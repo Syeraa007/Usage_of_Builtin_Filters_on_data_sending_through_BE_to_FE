8046e74e5e83d3f792d25dd679e38c926660a242
SYERAA007 <simhasimha4321.com>
2023-07-15 14:59:53 +0530

    first commit

--- App/views.py
+++ App/views.py
@@ -4,5 +4,5 @@ from django.shortcuts import render
 import datetime
 def buitlin_filters(request):
     dates=datetime.datetime.now()
-    d={'data':'THIS is WEEkenD  NoW we HaVe MoCK ShootING','count':12,'dates':dates}
+    d={'data':'ThiS is WEEkenD  NoW we HaVe MoCK ShootING','count':10,'dates':dates,'mine':'hiihii how are You daRinliG'}
     return render(request,'buitlin_filters.html',d)
